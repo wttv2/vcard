10f884a095dbfaf4ddb4be9d94c4208829b35538
Charl <webtubetv.net@gmail.com>
2026-03-31 17:58:23 -0600

    agregada funcion

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ document.getElementById("counter").innerText = visits;
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js");
 }
+
+function downloadVCF() {
+  const vcfData = `BEGIN:VCARD
+VERSION:3.0
+N:Cardoza;Armando
+FN:Armando Cardoza
+TITLE:Desarrollador Android / Transporte Ejecutivo
+TEL:+50371018695
+TEL:+50371991662
+EMAIL:arcardev77@gmail.com
+END:VCARD`;
+
+  const blob = new Blob([vcfData], { type: "text/vcard" });
+  const url = window.URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "Armando_Cardoza.vcf";
+  a.click();
+
+  window.URL.revokeObjectURL(url);
+}
